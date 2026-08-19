a7bbfd25fcfe555740928c62d9b43ded627acfbb
Tedesqui2027 <tedesqui2022@gmail.com>
2026-08-19 11:42:58 -0300

    Change price of 'combo-all' e-book package
    Updated the price of the 'combo-all' e-book package from 99.90 to 49.90.

--- api/checkout.js
+++ api/checkout.js
@@ -10,7 +10,7 @@ const catalogo = {
     'ebook-7': { titulo: 'Alimentados pela Palavra de Deus', preco: 19.90 },
     'ebook-8': { titulo: 'Casamento Segundo a Bíblia', preco: 19.90 },
     'ebook-9': { titulo: 'Libertados pelo Poder de Deus', preco: 19.90 },
-    'combo-all': { titulo: 'Combo 9 E-books Cristãos', preco: 99.90 }
+    'combo-all': { titulo: 'Combo 9 E-books Cristãos', preco: 49.90 }
 };
 
 module.exports = async function handler(req, res) {
